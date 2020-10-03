fde6927797bf20594a5d5d6ced874f7d3fab02db
Pextro <70188351+Pextro@users.noreply.github.com>
2020-10-03 17:31:22 -0300

    atualização do projeto
    correção no checkPoint,Barra de vida, Jump.

--- Assets/meu projeto/Scripts/CheckPoint.cs
+++ Assets/meu projeto/Scripts/CheckPoint.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
